6ef7a6cc49c30bc63ea8ad56073375911b2dc5b4
AlaaElkhamisy <wkood1108@gmail.com>
2024-11-25 18:41:11 +0200

    sliding text

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class AppTextStyles {
   static const appName_Style = TextStyle(
-      fontSize: 40,
+      fontSize: 60,
       color: Colors.white,
       fontFamily: "Pacifico",
       fontWeight: FontWeight.bold);
--- lib/features/splash/presentation/views/splash_body.dart
+++ lib/features/splash/presentation/views/splash_body.dart
@@ -1,30 +1,55 @@
 import 'package:bookly/core/utils/app_strings.dart';
 import 'package:bookly/core/utils/app_text_styles.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class Splash_Body extends StatelessWidget {
+class Splash_Body extends StatefulWidget {
   const Splash_Body({super.key});
 
   @override
+  State<Splash_Body> createState() => _Splash_BodyState();
+}
+
+// SingleTickerProviderStateMixin بتوفر انسيابيه ال Animation  وانه يحصل واحدة واحدة
+class _Splash_BodyState extends State<Splash_Body>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  // there i'll create animation object take the value from the animation controller and provide me flexability because controller gives you value from 0 to 1 which not satisfy all animations
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    // slidingAnimation.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(
+          const Center(
             child: Text(
               AppStrings.appName,
               style: AppTextStyles.appName_Style,
             ),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Read free books",
-            textAlign: TextAlign.center,
-          )
+          // const SizedBox(
+          //   height: 4,
+          // ),
+          // we added animation builder because the set state update the hole splash ui i not the animated text widget
+          SlidingText(slidingAnimation: slidingAnimation)
         ],
       ),
     );
