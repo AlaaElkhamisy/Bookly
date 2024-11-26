b3f9bc86e7d38ba25b00280ce605676cdc906393
AlaaElkhamisy <wkood1108@gmail.com>
2024-11-26 21:06:50 +0200

    app text styles

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 
 abstract class AppTextStyles {
   static const appName_Style = TextStyle(
-      fontSize: 60,
-      // color: Colors.white,
-      fontFamily: "Pacifico",
-      fontWeight: FontWeight.bold);
+      fontSize: 60, fontFamily: "Pacifico", fontWeight: FontWeight.bold);
 
-  static const primaryText_Style = TextStyle(
-      fontSize: 23,
-      // color: Colors.white,
+  static const primaryText_Style =
+      TextStyle(fontSize: 23, fontWeight: FontWeight.w600);
 
-      fontWeight: FontWeight.w600);
+  static const title_Style = TextStyle(
+      fontSize: 30, fontFamily: "GT-Sectra-Fine", fontWeight: FontWeight.bold);
+
+  static const text_Style14 =
+      TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+
+  static const text_Style20 =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
+
+  static const text_Style16 =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
 }
--- lib/features/home/presentation/views/widgets/home_body.dart
+++ lib/features/home/presentation/views/widgets/home_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/features/home/presentation/views/widgets/bestSeller.dart';
 import 'package:bookly/features/home/presentation/views/widgets/customAppBar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -14,7 +15,36 @@ class Home_Body extends StatelessWidget {
         Custom_App_Bar(),
         Featured_Book_List_View(),
         Best_Seller_Text(),
+        Best_Seller_ListView()
       ],
+    );
+  }
+}
+
+class Best_Seller_ListView extends StatelessWidget {
+  const Best_Seller_ListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
+                  image: const DecorationImage(
+                      image: AssetImage(Assets_Data.book1), fit: BoxFit.fill)),
+            ),
+          ),
+          Column(
+            children: [],
+          )
+        ],
+      ),
     );
   }
 }
