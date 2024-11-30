fd80cd79a797c32a5a2c17ccd9ef929e36d1c3b3
AlaaElkhamisy <wkood1108@gmail.com>
2024-11-30 17:04:39 +0200

    search result list view

--- lib/features/search/presentation/views/widgets/search_body.dart
+++ lib/features/search/presentation/views/widgets/search_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly/core/utils/app_text_styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/bestsellerListview_item.dart';
 import 'package:bookly/features/search/presentation/views/widgets/customSearchtext_field.dart';
+import 'package:bookly/features/search/presentation/views/widgets/search_result_listview.dart';
 import 'package:flutter/material.dart';
 
 class Search_Body extends StatelessWidget {
@@ -29,28 +29,6 @@ class Search_Body extends StatelessWidget {
           Expanded(child: Search_Result())
         ],
       ),
-    );
-  }
-}
-
-class Search_Result extends StatelessWidget {
-  const Search_Result({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      // i tell listView to take the height of its children
-      shrinkWrap: true,
-      //here we tell listView to not scroll because the customScrollView will scroll instead
-      //physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return const Padding(
-          padding: EdgeInsets.only(bottom: 20),
-          child: Best_Seller_ListView_item(),
-        );
-      },
     );
   }
 }
