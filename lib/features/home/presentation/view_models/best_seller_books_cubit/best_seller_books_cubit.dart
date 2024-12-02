1ce62330627d05417ae5317f17dd13829cf0c65d
AlaaElkhamisy <wkood1108@gmail.com>
2024-12-02 19:17:15 +0200

    best seller books cubit

--- lib/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_cubit.dart
+++ lib/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_cubit.dart
@@ -3,18 +3,18 @@ import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
-part 'featured_books_cubit_state.dart';
+part 'best_seller_books_state.dart';
 
-class FeaturedBooksCubitCubit extends Cubit<FeaturedBooksCubitState> {
-  FeaturedBooksCubitCubit(this.homeRepo) : super(FeaturedBooksInitial());
+class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
   final HomeRepo homeRepo;
-  Future<void> fetchFeaturedBooks() async {
-    emit(FeaturedBooksLoading());
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestSellerBooksLoading());
     var result = await homeRepo.fetchFeaturedBooks();
     result.fold((failure) {
-      emit(FeaturedBooksFailure(failure.errMessage));
+      emit(BestSellerBooksFailure(failure.errMessage));
     }, (books) {
-      emit(FeaturedBooksSuccess(books));
+      emit(BestSellerBooksSuccess(books));
     });
   }
 }
---
+++ lib/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_state.dart
@@ -0,0 +1,24 @@
+part of 'best_seller_books_cubit.dart';
+
+sealed class BestSellerBooksState extends Equatable {
+  const BestSellerBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class BestSellerBooksInitial extends BestSellerBooksState {}
+
+class BestSellerBooksLoading extends BestSellerBooksState {}
+
+class BestSellerBooksSuccess extends BestSellerBooksState {
+  final List<BookModel> books;
+
+  const BestSellerBooksSuccess(this.books);
+}
+
+class BestSellerBooksFailure extends BestSellerBooksState {
+  final String errMessage;
+
+  const BestSellerBooksFailure(this.errMessage);
+}
