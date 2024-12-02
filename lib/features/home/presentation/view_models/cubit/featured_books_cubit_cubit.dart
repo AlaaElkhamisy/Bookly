3960f49da845f1065016f5140b3aee523a6c240a
AlaaElkhamisy <wkood1108@gmail.com>
2024-12-02 19:07:27 +0200

    create featured books cubit

--- lib/features/home/presentation/view_models/cubit/featured_books_cubit_cubit.dart
+++ lib/features/home/presentation/view_models/cubit/featured_books_cubit_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_cubit_state.dart';
 
 class FeaturedBooksCubitCubit extends Cubit<FeaturedBooksCubitState> {
-  FeaturedBooksCubitCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubitCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
