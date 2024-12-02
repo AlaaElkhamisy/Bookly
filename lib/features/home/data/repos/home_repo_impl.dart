95ad70db81a4db00b271a0d342b18693ac515af7
AlaaElkhamisy <wkood1108@gmail.com>
2024-12-02 18:04:58 +0200

    implement fetch bestSeller books method

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest");
+      List<BookModel> books = [];
+      //in this for loop i told it to go to Map data and go to items , then return data as BookModel
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      // here if we don't return the books list as the right side of either this may give an error because we told it that the right side is the sccuessed state
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
